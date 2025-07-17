105c74992dfd205048fad850a293aa5faebb6c1b
Edson Morgado <edsonmorgadoboane15@gmail.com>
2025-07-17 23:15:34 +0200

    Added code

--- T1.py
+++ T1.py
@@ -3,4 +3,15 @@ N1=int(input('N:'))
 N2=int(input('N2:'))
 print ('Your result is:{}'.format( math.cos(N1) + N2))
 #This is just a little test. 
+
+#2
+import random 
+print ('Tell me your names:')
+N1=str(input('Name 1:'))
+N2=str(input('Name 2:'))
+N3=str(input('Name 3:'))
+list= [N1,N2,N3]
+S= random.choice(list) 
+print ('The choosen is: {}'.format(S))
+
        
